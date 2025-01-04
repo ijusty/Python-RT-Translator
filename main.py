9608dc3dc36a951725bedaae5c6c59db472e6c96
ijusty <rambl4@inbox.ru>
2025-01-04 20:30:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,32 +2,46 @@ import pyperclip
 import keyboard
 import time
 from deep_translator import GoogleTranslator
+import tkinter as tk
+from tkinter import simpledialog, messagebox
 
 def translate_text(text, source_language='auto', target_language='en'):
     try:
+        # Перевод текста
         translated = GoogleTranslator(source=source_language, target=target_language).translate(text)
         return translated
     except Exception as e:
-        print("Ошибка при переводе:", e)
-        return text
+        print("Error during translation:", e)
+        return text 
 
 def on_copy(source_language, target_language):
+    time.sleep(0.1)
     text = pyperclip.paste()
-    print("Текст для перевода: ", text)
+    
     if text:
         translated_text = translate_text(text, source_language, target_language)
         pyperclip.copy(translated_text)
-        print("Переведенный текст:", translated_text)
+        print("Translated text copied to clipboard:", translated_text)
+        
         keyboard.press_and_release('ctrl+v')
 
 def start_translator():
-    source_language = input("Введите код языка, с которого переводить (например, 'ru' для русского): ")
-    target_language = input("Введите код языка, на который переводить (например, 'en' для английского): ")
+    root = tk.Tk()
+    root.withdraw() 
+
+    source_language = simpledialog.askstring("Выбор языка", "Введите код языка, с которого переводить (например, 'ru'):")
+    target_language = simpledialog.askstring("Выбор языка", "Введите код языка, на который переводить (например, 'en'):")
+
+
+    main_window = tk.Tk()
+    main_window.title("Переводчик")
+    main_window.iconbitmap('')
 
     keyboard.add_hotkey('ctrl+c', lambda: on_copy(source_language, target_language))
 
-    print("Переводчик работает.")
+    print("Press Ctrl+C to translate selected text...")
     keyboard.wait()
+    main_window.mainloop()
 
 if __name__ == "__main__":
     start_translator()
